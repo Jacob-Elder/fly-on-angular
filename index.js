a875d4859ff3bbd57f2431552d57f06b63f284e5
Jacob-Elder <jacobelderonly@gmail.com>
2016-11-02 09:06:25 -0700

    form functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
+app.use(express.static(__dirname + '/public/'));
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/airplanes');
@@ -11,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('morgan')('dev'));
 
 app.use('/api/airplanes', require('./controllers/airplane'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 var server = app.listen(process.env.PORT || 3000);
 
---
+++ public/js/app.js
@@ -0,0 +1,55 @@
+var app = angular.module('UserApp', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+  function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+      .state('plane', {
+        url: '/',
+        controller: 'PlaneCtrl',
+        templateUrl: 'app/planes.html'
+      })
+      .state('404', {
+        url: '/404',
+        controller: '',
+        template: '<h1>404</h1>'
+      })
+
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/404');
+}])
+
+app.controller('PlaneCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.plane = {
+    manufacturer: '',
+    model: '',
+    engines: 0,
+    picture: ''
+  }
+
+  $http.get('/api/airplanes').then(function success(res) {
+    $scope.planes = res.data
+  }, function error(res) {
+    console.log(res);
+  })
+
+  $scope.delete = function(id, idx) {
+    var route = '/api/airplanes/' + id
+    $http.delete(route)
+         .then(function success(res) {
+            $scope.planes.splice(idx, 1)
+            console.log(res);
+          }, function error(res) {
+            console.log(res);
+          })
+  }
+
+  $scope.formSubmit = function() {
+    $http.post('/api/airplanes', $scope.plane)
+         .then(function success(res) {
+            $scope.planes.push(res.data)
+         }, function error(err) {
+          console.log(err);
+         });
+  }
+
+}]);
